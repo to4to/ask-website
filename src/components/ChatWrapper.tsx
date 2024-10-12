36ef145ce4c9943379df96117f0af0cae65fccd5
to4to <justjustifying@outlook.com>
2024-10-13 01:20:39 +0530

    Refactor ChatWrapper to accept sessionID and configure useChat API

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -1,10 +1,18 @@
-"use client"
+"use client";
 
-import { useChat } from "ai/react"
+import { useChat } from "ai/react";
 
-
-export const ChatWrapper = () = {
-
-    const {}=useChat()
+export const ChatWrapper = ({ sessionID }: { sessionID: string }) => {
+  const {} = useChat({
+      api: "/api/chat-stream",
+      body:{sessionID}
+  });
     
-}
+    
+    
+    
+    return (
+        <div className="relative bg-zinc-50 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
+        </div>
+    )
+};
