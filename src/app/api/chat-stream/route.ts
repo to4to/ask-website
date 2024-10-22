c94da81166924ef1d95fd95dd7dad94525a699a9
to4to <justjustifying@outlook.com>
2024-10-23 00:33:50 +0530

    Enhance chat-stream API to support streaming responses with sessionId

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -1,12 +1,15 @@
 import { ragChat } from "@/lib/rag-chat";
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest } from "next/server";
 
-export const POST = async (req:NextRequest) => {
+export const POST = async (req: NextRequest) => {
     const { messages, sessionId } = await req.json()
-    
+
     const lastMessage = messages[messages.length - 1].content
-    
-    const response = await ragChat.chat(lastMessage)
-    
-    console.log("response",response)
+
+    const response = await ragChat.chat(lastMessage, { streaming: true, sessionId })
+
+    // console.log("response", response)
+
+    return aiUseChatAdapter
 }
